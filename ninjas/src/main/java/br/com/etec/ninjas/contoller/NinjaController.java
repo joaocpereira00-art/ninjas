f866785d2c14be06fb6ebc25e81baf23cacf105a
joao <joaocpereira00@gmail.com>
2026-06-09 22:40:58 -0300

    List

--- ninjas/src/main/java/br/com/etec/ninjas/contoller/NinjaController.java
+++ ninjas/src/main/java/br/com/etec/ninjas/contoller/NinjaController.java
@@ -1,6 +1,9 @@
 package br.com.etec.ninjas.contoller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,4 +25,8 @@ public class NinjaController {
         
        }
 
+       @GetMapping
+      public List<Ninja> listarNinjas(){
+        return ninjasService.listarNinjas();
+    }
 }
--- ninjas/src/main/java/br/com/etec/ninjas/service/NinjasService.java
+++ ninjas/src/main/java/br/com/etec/ninjas/service/NinjasService.java
@@ -1,5 +1,7 @@
 package br.com.etec.ninjas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.etec.ninjas.model.Ninja;
@@ -10,8 +12,13 @@ public class NinjasService {
     
     @Autowired
     private NinjaRepository ninjaRepository ; 
-
+    // srve para colocar um cadastro por vez
     public Ninja cadastrarNinja (Ninja ninja){
         return ninjaRepository.save(ninja);
     }
+    // serve para colocar varios cadastros
+    public List<Ninja> listarNinjas(){
+        return ninjaRepository.findAll();
+    }
+    
 }
